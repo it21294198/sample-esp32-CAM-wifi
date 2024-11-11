5a1a3939e81d3ab0f6b82898f6cad4fbb2eb74e9
it21294198 <it21294198@my.sliit.lk>
2024-11-11 23:28:28 +0530

    http-GET-deserialization

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Main Branch --> Basic Setup for WiFi connection.
 
 2. sample-http-GET --> By using `https://jsonplaceholder.typicode.com/posts/` public URL HTTP GET request is send to get random post between 1 and 10.
+
+3. http-GET-deserialization --> Deserialize the data from `https://jsonplaceholder.typicode.com/posts/` integer id and string title.
--- sample-esp32-wifi.ino
+++ sample-esp32-wifi.ino
@@ -1,9 +1,9 @@
 #include <WiFi.h>
-#include <HTTPClient.h>;
-#include <ArduinoJson.h>;
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-// const char *ssid = "";
-// const char *password = "";
+const char *ssid = "";     // WiFi SSID
+const char *password = ""; // WiFi Password
 
 void setup()
 {
@@ -16,34 +16,53 @@ void setup()
     Serial.print(".");
     delay(500);
   }
-  Serial.print("\nConnected to the WiFi Network IP :");
-  Serial.print(WiFi.localIP());
+  Serial.print("\nConnected to the WiFi Network IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-  if ((WiFi.status() == WL_CONNECTED))
+  if (WiFi.status() == WL_CONNECTED)
   {
     long random_number = random(1, 10);
     HTTPClient client;
 
-    client.begin("https://jsonplaceholder.typicode.com/posts/" + String(random_number));
+    String url = "https://jsonplaceholder.typicode.com/posts/" + String(random_number);
+    client.begin(url);
     int http_status_code = client.GET();
 
     if (http_status_code > 0)
     {
       String payload = client.getString();
-      Serial.println("\nStatus Code : " + String(http_status_code));
-      Serial.println(payload);
+      Serial.println("\nStatus Code: " + String(http_status_code));
+      Serial.println("Payload:\n" + payload);
+
+      StaticJsonDocument<500> doc;
+      DeserializationError error = deserializeJson(doc, payload);
+
+      if (!error)
+      {
+        int id = doc["id"];
+        const char *title = doc["title"];
+
+        Serial.println("Id: " + String(id));
+        Serial.println("Title: " + String(title));
+      }
+      else
+      {
+        Serial.println("Failed to parse JSON");
+      }
+
+      client.end();
     }
     else
     {
-      Serial.print("Error on HTTP request");
+      Serial.println("Error on HTTP request");
     }
   }
   else
   {
-    Serial.print("Connection Lost");
+    Serial.println("Connection Lost");
   }
-  delay(10000);
+  delay(10000); // Wait 10 seconds before the next request
 }
